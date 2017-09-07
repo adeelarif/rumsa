d4944c3719fc7693ce855ac8951f7eeb1d7d6700
Muhammad Arif <adeelarif66@yahoo.com>
2017-09-07 14:52:28 -0400

    contact-us, event addition done
    Awaiting eboard photos

--- js/main.js
+++ js/main.js
@@ -69,3 +69,21 @@ jQuery(function($) {'use strict',
 		social_tools: false
 	});
 });
+//Event grid animation
+$( document ).ready(function() {
+    $("[rel='tooltip']").tooltip();    
+ 
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    ); 
+});
+
+
+
+
+
